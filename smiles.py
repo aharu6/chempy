b8b4464040eda78e7cce10d01eefc40836dcf057
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-04-22 10:21:35 +0900

    記録

--- smiles.py
+++ smiles.py
@@ -1,7 +1,10 @@
 !pip install pandas
+!pip install rdkit
 import csv
 import pandas as pd
+import rdkit as rd
 
+#読み込み
 cf = pd.read_csv("/Users/aizawaharuka/Library/CloudStorage/OneDrive-学校法人明治薬科大学/春香/2023/AID_743240_datatable_all.csv")
 cf.head()
 print(cf.columns.to_list())
@@ -13,5 +16,6 @@ print(cf['PUBCHEM_EXT_DATASOURCE_SMILES'])
 cf2 = cf.assign(PUBCHEM_EXT_DATASOURCE_SMILES = cf['PUBCHEM_EXT_DATASOURCE_SMILES'].str.split('.')).explode('PUBCHEM_EXT_DATASOURCE_SMILES')
 print(cf2['PUBCHEM_EXT_DATASOURCE_SMILES'])
 
+#塩を除く
 cf3 = cf2.assign(smile_len = cf2['PUBCHEM_EXT_DATASOURCE_SMILES'].str.len())
-cf3
+cf4 = cf3.groupby('PUBCHEM_RESULT_TAG').filter(axis = "index",like = cf3.groupby("PUBCHEM_RESULT_TAG")["smile_len"].max())
